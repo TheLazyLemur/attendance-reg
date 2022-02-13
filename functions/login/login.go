bb09a0b90386726583bbf1f7024cda117d397c28
Dan <danrousseau@protonmail.com>
2022-02-13 22:21:58 +0200

    :sparkles: (auth) login endpoint added

---
+++ functions/login/login.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+	"os"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	token := os.Getenv("supabase_token")
+
+	payload := fmt.Sprintf("{\"token\":\"%s\"}", token)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              payload,
+		IsBase64Encoded:   false,
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call
+	lambda.Start(handler)
+}
