c2b94e3123de9e9f412ea8e7d72538b2eddb1569
Dan <danrousseau@protonmail.com>
2022-02-14 17:28:08 +0200

    :bug: (email) fix payload

--- Envoys/EmailEnvoy.cs
+++ Envoys/EmailEnvoy.cs
@@ -1,15 +1,29 @@
+using System.Net.Http.Headers;
+
 namespace attendance_reg.Pages.Envoys;
 
 public class EmailEnvoy
 {
    public async Task Send(string to, string subject, string content)
    {
-      var httpClient = new HttpClient();
-
-      var result = await httpClient.GetAsync(new Uri($"https://holborn-za-attendance.netlify.app/.netlify/functions/email?to={to}&subject={subject}&content={content}"));
-
-      result.EnsureSuccessStatusCode();
-      
-      var response = await result.Content.ReadAsStringAsync();
+      var client = new HttpClient();
+      var request = new HttpRequestMessage
+      {
+         Method = HttpMethod.Post,
+         RequestUri = new Uri("https://holborn-za-attendance.netlify.app/.netlify/functions/email"),
+         Content = new StringContent($"{{\n    \"personalizations\": [\n        {{\n            \"to\": [\n                {{\n                    \"email\": \"{to}\"\n                }}\n            ],\n            \"subject\": \"{subject}\"\n        }}\n    ],\n    \"from\": {{\n        \"email\": \"from_address@example.com\"\n    }},\n    \"content\": [\n        {{\n            \"type\": \"text/plain\",\n            \"value\": \"{content}\"\n        }}\n    ]\n}}")
+         {
+            Headers =
+            {
+               ContentType = new MediaTypeHeaderValue("application/json")
+            }
+         }
+      };
+      using (var response = await client.SendAsync(request))
+      {
+         response.EnsureSuccessStatusCode();
+         var body = await response.Content.ReadAsStringAsync();
+         Console.WriteLine(body);
+      }
    }
 }
